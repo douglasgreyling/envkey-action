6243608273bec70a6f11e92a9c2a98f28ce19ac5
Fritz Larco <flarco@live.com>
2023-09-03 20:17:39 -0300

    remove quotes

--- envkey-load.py
+++ envkey-load.py
@@ -4,7 +4,7 @@ secrets = envkey.fetch_env(os.getenv('ENVKEY'), cache_enabled=False)
 
 bash_lines = ['#!/usr/bin/env bash', '']
 for k, v in secrets.items():
-  bash_lines.append(f"""echo '{k}="{v}"' >> $GITHUB_ENV""")
+  bash_lines.append(f"""echo '{k}={v}' >> $GITHUB_ENV""")
 
 with open('/tmp/secrets.sh', 'w') as file:
   file.write('\n'.join(bash_lines))
