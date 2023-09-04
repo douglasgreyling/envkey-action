dd67268414c84d978f732b7d92b70d8c8f377a36
Fritz Larco <flarco@live.com>
2023-09-03 21:07:04 -0300

    add single quote for env_lines

--- envkey-load.py
+++ envkey-load.py
@@ -6,7 +6,7 @@ bash_lines = ['#!/usr/bin/env bash', '']
 env_lines = []
 for k, v in secrets.items():
   bash_lines.append(f"""echo '{k}={v}' >> $GITHUB_ENV""")
-  env_lines.append(f"""{k}={v}""")
+  env_lines.append(f"""{k}='{v}'""")
 
 with open('/tmp/secrets.sh', 'w') as file:
   file.write('\n'.join(bash_lines))
