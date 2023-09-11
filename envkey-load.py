2e959bea0396a5cf69be9c1ef546912be7401237
Fritz Larco <flarco@live.com>
2023-09-11 08:06:00 -0400

    Update envkey-load.py

--- envkey-load.py
+++ envkey-load.py
@@ -8,6 +8,14 @@ for k, v in secrets.items():
   bash_lines.append(f"""echo '{k}={v}' >> $GITHUB_ENV""")
   env_lines.append(f"""{k}='{v}'""")
 
+# replace re-used env vars
+for k, val in secrets.items():
+  key = '${' + k + '}'
+  for i, bash_line in enumerate(bash_lines):
+    bash_lines[i] = bash_line.replace(key, val)
+  for i, env_line in enumerate(env_lines):
+    env_lines[i] = env_line.replace(key, val)
+
 with open('/tmp/secrets.sh', 'w') as file:
   file.write('\n'.join(bash_lines))
 
